9bd954ce5eff164918c2d810107ea3506162ea12
barmank32 <barmank@yandex.ru>
2021-04-25 17:59:33 +0300

    mini update

--- terraform/files/loging.sh
+++ terraform/files/loging.sh
@@ -5,10 +5,8 @@ echo "Stop Docer and yc-container-daemon"
     sudo service yc-container-daemon stop
 
 echo "Copy configs"
-        # sudo mkdir -p /volume/prometheus/data
-    sudo cp -r -t /volume/ /tmp/loging/*
-        # sudo chown -R 472:1 /volume/grafana
-        # sudo chmod -R 777 /volume/prometheus/data
+        sudo mkdir -p /volume/
+        sudo cp -r -t /volume/ /tmp/loging/*
 
 echo "Start Docer and yc-container-daemon"
     sudo service docker start
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,12 +1,12 @@
 output "Crawler_ui" {
-  value = "http://${yandex_compute_instance.crawler-ui.network_interface.0.nat_ip_address}:8000"
+  value = "http://${yandex_compute_instance.nginx.network_interface.0.nat_ip_address}"
 }
 output "Rabbitmq_management" {
-  value = "http://${yandex_compute_instance.rabbitmq.network_interface.0.nat_ip_address}:15672"
+  value = "http://${yandex_compute_instance.nginx.network_interface.0.nat_ip_address}:8080"
 }
 output "Prometheus" {
-  value = "http://${yandex_compute_instance.monitoring.network_interface.0.nat_ip_address}:9090"
+  value = "http://${yandex_compute_instance.nginx.network_interface.0.nat_ip_address}:9090"
 }
 output "Grafana" {
-  value = "http://${yandex_compute_instance.monitoring.network_interface.0.nat_ip_address}:3000"
+  value = "http://${yandex_compute_instance.nginx.network_interface.0.nat_ip_address}:3000"
 }
